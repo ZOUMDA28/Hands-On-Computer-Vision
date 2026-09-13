aac2b4107cc2a5148226a518dc49b64f65d92051
MARTIN ZOU <zoumda28@gmail.com>
2026-09-13 20:08:54 +0800

    feat: add tutorial website files

---
+++ web/src/config.js
@@ -0,0 +1,24 @@
+export const config = {
+  site: {
+    title: 'Hands-On Computer Vision',
+    subtitle: '计算机视觉自学与实践',
+    description: '从像素基础到三维重建的编程实践教程',
+    author: 'zoumda28',
+    repo: 'https://github.com/zoumda28/Hands-On-Computer-Vision'
+  },
+  features: {
+    bilingual: true,
+    darkMode: true,
+    notes: true,
+    bookmarks: true,
+    textSelectionToolbar: true,
+    codeFolding: true,
+    imageLightbox: true,
+    guidedTour: true,
+    changelog: true
+  },
+  navigation: {
+    defaultView: 'sidebar', // 'sidebar' | 'welcome'
+    showProgress: true
+  }
+}
